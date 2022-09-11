bfd6793ee4abcd8fc724dddb4e17965cbbfaf77f
Aaron Lam <aaronlam2k@gmail.com>
2022-09-11 12:25:40 -0700

    Sanity hookup, Create homepage with product elements pulled from Sanity

---
+++ pages/components/Product.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../../lib/client';
+
+function Product({ product: { image, name, slug, price } }) {
+   return (
+      <div>
+         <div className='product-card'>
+            <Link href={`/products/${slug.current}`}>
+               <img src={urlFor(image && image[0])} 
+               className='product-image' 
+               width={250}
+               height={250}
+               />
+            </Link>
+         </div>
+         <h4 className='product-name'>{name}</h4>
+         <p className='product-price'>${price}</p>
+      </div>
+   );
+}
+
+export default Product;
