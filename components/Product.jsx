5a7aa31646cab4d6247452fd800ae690a1765356
Aaron Lam <aaronlam2k@gmail.com>
2022-09-14 21:38:49 -0700

    Bug fix: Entire product card redircts to page now

--- components/Product.jsx
+++ components/Product.jsx
@@ -5,23 +5,23 @@ import { urlFor } from '../lib/client';
 
 function Product({ product: { image, name, slug, price } }) {
    return (
-      <div className='product-card'>
-         <div className='image-wrapper'>
-            <div className='image-container'>
-               <Link href={`/products/${slug.current}`}>
+      <Link href={`/products/${slug.current}`}>
+         <div className='product-card'>
+            <div className='image-wrapper'>
+               <div className='image-container'>
                   <img src={urlFor(image && image[0])}
                      className='product-image image-fit'
                   />
-               </Link>
+               </div>
+            </div>
+
+
+            <div className='details'>
+               <h4 className='product-name'>{name}</h4>
+               <p className='product-price'>${price}</p>
             </div>
          </div>
-
-
-         <div className='details'>
-            <h4 className='product-name'>{name}</h4>
-            <p className='product-price'>${price}</p>
-         </div>
-      </div>
+      </Link>
    );
 }
 
