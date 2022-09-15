fd92d4d21f0d1ac2d6b236ec7880e49c75941741
Aaron Lam <aaronlam2k@gmail.com>
2022-09-15 11:18:56 -0700

    Stripe more ship options, allow render of png & jpg images

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,21 +4,22 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
    if (req.method === 'POST') {
-
-      console.log(req.body);
-
       const params = {
          submit_type: 'pay',
          mode: 'payment',
          payment_method_types: ['card'],
          billing_address_collection: 'auto',
          shipping_options: [
-            { shipping_rate: 'shr_1LfXsDD9HC8B9KWBHh1wrRPf' }, //Fast Shipping
-            { shipping_rate: 'shr_1LfXrAD9HC8B9KWBsIfg3aIF' }, //Free Shipping
+            { shipping_rate: 'shr_1LiMQzD9HC8B9KWB614AM8NK' }, //Free Shipping
+            { shipping_rate: 'shr_1LiMQ8D9HC8B9KWBqgjmbI0F' }, //Express Shipping
+            { shipping_rate: 'shr_1LiMQiD9HC8B9KWBXbyBQMkO' }, //Fast Shipping
          ],
          line_items: req.body.map((item) => {
             const img = item.image[0].asset._ref;
-            const newImage = img.replace('image-', 'https://cdn.sanity.io/images/43iv1y2w/production/').replace('-webp', '.webp');
+            const newImage = img.replace('image-', 'https://cdn.sanity.io/images/43iv1y2w/production/')
+               .replace('-webp', '.webp')
+               .replace('-jpg', '.jpg')
+               .replace('-png', '.png');
 
             return {
                price_data: {
